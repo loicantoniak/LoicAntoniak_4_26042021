2c32485b21a9a14fa403cd223db8aafc505cd22a
Antoniak Loïc <loic.antoniak@gmail.com>
2021-04-30 12:28:37 +0200

    add eventClick on closeModal

--- GameOne - Loic Antoniak/modal.js
+++ GameOne - Loic Antoniak/modal.js
@@ -1,16 +1,27 @@
-function editNav() {
-  var x = document.getElementById("myTopnav");
-  if (x.className === "topnav") {
-    x.className += " responsive";
-  } else {
-    x.className = "topnav";
-  }
-}
+"use strict";
 
 // DOM Elements
 const modalbg = document.querySelector(".bground");
 const modalBtn = document.querySelectorAll(".modal-btn");
 const formData = document.querySelectorAll(".formData");
+const closeBtn = document.querySelector(".close");
+const myTopnav = document.getElementById("myTopnav")
+
+/*========================*/
+/*      RESPONSIVE NAV    */
+/*========================*/
+
+function editNav() {
+  if (myTopnav.className === "topnav") {
+    myTopnav.className += " responsive";
+  } else {
+    myTopnav.className = "topnav";
+  }
+}
+
+/*========================*/
+/*      LAUNCH MODAL      */
+/*========================*/
 
 // launch modal event
 modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
@@ -20,4 +31,14 @@ function launchModal() {
   modalbg.style.display = "block";
 }
 
+/*========================*/
+/*      CLOSE MODAL       */
+/*========================*/
 
+// close modal form
+const closeModal = () => {
+  modalbg.style.display = "none";
+};
+
+// close modal event
+closeBtn.addEventListener("click", closeModal);
